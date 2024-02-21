630f6ff504718b087fad2f98c1bdc189cac3b42b
amitabhisgood <amitabhisgood@gmail.com>
2024-02-21 15:41:42 +0530

    New

---
+++ HelloComponent.js
@@ -0,0 +1,12 @@
+// HelloComponent.js
+import React from 'react';
+
+const HelloComponent = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default HelloComponent;
